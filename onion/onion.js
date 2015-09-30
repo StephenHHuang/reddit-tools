059ec649fc3ff48fb686bb1c55b162b5af185f67
Stephen Huang <s.huangbiz@gmail.com>
2015-09-30 09:56:30 +0000

    started onion.js

--- onion/onion.js
+++ onion/onion.js
@@ -1,7 +1,45 @@
-$(document).ready(function(){
-    $("#startbutton").click(function(){
-       $(this).hide('slide');
-       $("h1").fadeOut("slow");
+function generateRandom(){
+    return Math.floor(Math.random()*(25));
+}
+
+function getHeadlines(){
+    var notTheOnion = "https://www.reddit.com/r/NotTheOnion/top/.json?sort=top&t=all";
+    //var post='';
+    $.getJSON(notTheOnion, function(json){
+        var listing = $(json.data.children);
+        var postNum = generateRandom();
+        console.log(postNum);
+        var post = listing[postNum].data;
+        //console.log(post);
+        return post;
+        
     });
+    
+    
+}
+
+var main = {
+    gameStart: function() {
+        getHeadlines(function(){
+            console.log("finished");
+        });
+        
+    }  
+};
+
+var splash = {
+    onReady: function() {
+        $('#startbutton').click(function(){
+            $(this).fadeOut('slow');
+            $("h1").fadeOut('slow');
+            $("#headline").toggle(1000);
+            $('#headline').html('<img src="https://media.giphy.com/media/IgQ8E05Dpg2ze/giphy.gif" class="center-block">');
+            main.gameStart();
+        });
+    }
+};
+
+$(document).ready(function(){
+    splash.onReady(); 
     
 });
